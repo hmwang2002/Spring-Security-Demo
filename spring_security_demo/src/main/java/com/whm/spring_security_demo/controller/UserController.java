801baa3bedf73fe651675085d440c6476c8dd1fa
wanghm16 <2509553015@qq.com>
2023-08-01 10:59:41 +0800

    controller改个名字

--- spring_security_demo/src/main/java/com/whm/spring_security_demo/controller/UserController.java
+++ spring_security_demo/src/main/java/com/whm/spring_security_demo/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2023/7/22 19:28
  */
 @RestController
-public class LoginController {
+public class UserController {
     @Autowired
     private LoginService loginService;
 
